d5d5876fa24552187adf924e6ab08b5735805990
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-10-08 23:07:35 -0400

    up

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const fretboardKit = require("@jasonfleischer/fretboard")
+const musicKit = require("@jasonfleischer/music-model-kit");
+musicKit.init();
+
+const fretboardView = fretboardKit({
+	id: 'your_fretboard_id',
+	width: 325,
+	onClick: function(note, isOn) {
+		if(isOn) {
+			fretboardView.drawNote(note);
+		} else {
+			fretboardView.clearNote(note);
+		}
+	},
+	hover: true
+});
+
+document.getElementById("note_button").onclick = function() { 
+	let midiValue = 45; // A2
+	let note = musicKit.all_notes[midiValue];
+	fretboardView.clear();
+	fretboardView.drawNote(note);
+};
+document.getElementById("chord_button").onclick = function() {
+	let midiValue = 60 // C4
+	let note = musicKit.all_notes[midiValue];
+	let chord = new musicKit.Chord(note, musicKit.Chord.TYPE.minor);
+	fretboardView.drawChord(chord);
+}
+document.getElementById("scale_button").onclick = function() {
+	let midiValue = 62 // D
+	let note = musicKit.all_notes[midiValue];
+	let scale = new musicKit.Scale(note, musicKit.Scale.TYPE.Aeolian);
+	fretboardView.drawScale(scale);
+}
+document.getElementById("clear_button").onclick = function() {
+	fretboardView.clear();
+}
+
+
+new musicKit.MidiListener(
+	function (midiValue, channel, velocity) { // note on
+		let note = musicKit.all_notes[midiValue];
+		fretboardView.drawNote(note);
+	},
+	function (midiValue, channel, velocity) { // note off
+		let note = musicKit.all_notes[midiValue];
+		fretboardView.clearNote(note);
+	});
+
---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# requirements:
+# npm, uglify-js, browserify
+
+# $ npm install -g uglify-js
+# $ npm install -g browserify
+
+CWD=`pwd`
+
+uglifyjs js/main.js -o js/bundle.js
+browserify js/bundle.js -o js/bundle.js
+uglifyjs js/bundle.js -o js/bundle.js
+
+git add *; git commit -m 'update'; git push;
+
+cd $CWD
+
+
